bfb5a381912360552ffdd4452ff35aed6e5e4697
Wayne Hong <woo.young.hong@hotmail.com>
2018-12-29 04:18:56 -0500

    added proxy to client/package.json, added heroku-postbuild script to package.json, modified server.js so that the app serves index.html

--- server.js
+++ server.js
@@ -5,12 +5,20 @@ const $PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('here!');
-});
+// Serve static files from the React frontend app (middleware)
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// app.get('/', (req, res, next) => {
+//   res.send('here!');
+// });
 
 app.get('/api', (req, res, next) => {
   res.send('API requests!');
+});
+
+// Anything that doesn't match the above, send back index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 app.listen($PORT, () => {
