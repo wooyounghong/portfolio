0b5a9d94c21171d83f063875ce16c7b8a2a6b88b
Wayne Hong <woo.young.hong@hotmail.com>
2019-01-31 17:39:00 +0900

    added resume  jpg file onto Resume component

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,9 +1,28 @@
 import React from 'react';
+// import { Document, Page } from 'react-pdf';
+import './css/Resume.css';
 
-const Resume = () => (
-  <div>
-    <div>Resume Component</div>
-  </div>
-);
+class Resume extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numPages: null,
+      pageNumber: 1,
+    };
+  }
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+  render() {
+    // const { pageNumber, numPages } = this.state;
+    const resumePDF = require('../image/resumeJPG.jpg');
 
+    return (
+      <div>
+        {/* <div className="center">Temorary Resume Component</div> */}
+        <img src={resumePDF} alt="resume-pdf" className="center-image" />
+      </div>
+    );
+  }
+}
 export default Resume;
