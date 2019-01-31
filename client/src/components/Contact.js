ac5139d2b56c0498580b1bc46bb3fb2ac9bcf2bf
Wayne Hong <woo.young.hong@hotmail.com>
2019-01-31 19:19:26 +0900

    Added social media links and email link

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 
+import './css/Contact.css';
 const Contact = () => (
-  <div>
-    <div>Contact Component</div>
+  <div className="contact-container">
+    <div className="contact-header">Contact ME!</div>
+    <img
+      src={require('../image/contact-me.jpg')}
+      alt="moses and i"
+      className="contact-image"
+    />
+    <div className="contact-footer">
+      <div className="contact-footer-items">
+        <SocialIcon
+          url="https://www.linkedin.com/in/wayne-wooyoung-hong/"
+          target="_blank"
+        />
+      </div>
+      <div className="contact-footer-items">
+        <SocialIcon
+          url="https://github.com/wooyounghong"
+          bgColor="black"
+          target="_blank"
+        />
+      </div>
+      <div className="contact-footer-items">
+        <SocialIcon
+          url="https://www.facebook.com/profile.php?id=605924668"
+          target="_blank"
+        />
+      </div>
+      <div className="contact-footer-items">
+        <a href={'mailto:hongwy1128@gmail.com'}>
+          <img src={require('../image/gmail.svg')} className="contact-gmail" />
+        </a>
+      </div>
+    </div>
   </div>
 );
 
