4ff7c9b46ff32450d23627b5afade63270028592
Wayne Hong <woo.young.hong@hotmail.com>
2019-02-02 20:52:52 +0900

    Added margin-left prop to image/video player

--- client/src/data/projectData.js
+++ client/src/data/projectData.js
@@ -1,8 +1,6 @@
-const projectsData = [
-  {
+const projectsData = [{
     title: 'NowOrLater',
-    description:
-      'Mobile messenger app that enables conditional SMS based on geolocation and/or timing',
+    description: 'Mobile messenger app that enables conditional SMS based on geolocation and/or timing',
     list: [
       'Designed responsive front-end using React and react-native enabled user authentication with firebase authentication',
       'Utilized Google maps API to enable geolocation and tracking of distance between destination and current location',
@@ -13,8 +11,7 @@ const projectsData = [
   },
   {
     title: 'Stock Analytics',
-    description:
-      'Analytical web app that renders real-time stock prices on to line charts with trend line overlay capabilities',
+    description: 'Analytical web app that renders real-time stock prices on to line charts with trend line overlay capabilities',
     list: [
       'Constructed front-end search bar using React form to search for companies and Semantic UI to create responsive layout',
       'Employed IEX API to filter company stock prices, and visualized real-time data using react-vis and socket.io',
@@ -25,8 +22,7 @@ const projectsData = [
   },
   {
     title: 'BathTub, inc',
-    description:
-      'E-commerce web application to shop for rubber ducks, successfully deployed using Heroku.',
+    description: 'E-commerce web application to shop for rubber ducks, successfully deployed using Heroku.',
     list: [
       'Provided customers with persisted cart sessions by applying React-Redux, Express, PostgresQL, and Sequelize',
       'Embedded Google OAuth authentication capabilities and created test cases implementing TDD',
